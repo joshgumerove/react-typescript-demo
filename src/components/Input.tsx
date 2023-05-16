cdc83506ae5d11f73120279faa273c6d89ad424e
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-05-16 13:20:08 -0400

    add event with types inside of component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,10 +1,16 @@
-import React, { FC } from "react";
+import React, { FC, ChangeEvent } from "react";
 import { InputProps } from "./InputProps";
 
 const Input: FC<InputProps> = ({ handleChange, value }) => {
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log("here is the event", e.target.value);
+  };
+
+  //   note: can define a function with types inside the component
   return (
     <>
       <input type="text" onChange={handleChange} />
+      <input type="text" onChange={handleInputChange} />
       <h2>{value ? value : "please type something in the input"}</h2>
     </>
   );
