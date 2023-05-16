a2e52ea678afdc6f9131bc80b287d731aa2fe7f5
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-05-16 14:39:57 -0400

    Type assertion in useState hook

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -3,21 +3,17 @@ import { AuthUser } from "./AuthUser";
 import { useState } from "react";
 
 const User: FC = () => {
-  const [user, setUser] = useState<AuthUser | null>(null);
+  const [user, setUser] = useState<AuthUser>({} as AuthUser);
   const handleLogin = () => {
     setUser({ name: "Josh", email: "Gumerove" });
-  };
-  const handleLogout = () => {
-    setUser(null);
   };
 
   return (
     <div>
       <button onClick={handleLogin}>Login</button>
-      <button onClick={handleLogout}>Logout</button>
       <div>
-        <p>User name is: {user?.name}</p>
-        <p>User email is: {user?.email}</p>
+        <p>User name is: {user.name}</p>
+        <p>User email is: {user.email}</p>
       </div>
     </div>
   );
